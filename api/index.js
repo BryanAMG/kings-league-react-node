ecab1eb80d3635f799ddf5cec0c2652c084e188d
Bryan Armando <mamanibryan1@gmail.com>
2023-02-20 12:42:01 -0500

    Add Server Static to images

--- api/index.js
+++ api/index.js
@@ -1,17 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 import leaderBoard from "../db/leaderBoard.json";
 import { Hono } from "hono";
+import { serveStatic } from "hono/serve-static.module";
+// import { serveStatic } from "hono/cloudflare-workers";
 
 const app = new Hono();
 
+app.get("/static/*", serveStatic({ root: "./" }));
 app.get("/", (ctx) => {
   return ctx.json([
     {
