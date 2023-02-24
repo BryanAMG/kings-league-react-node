71305ee03720c9b38ea97c7ba4af28bf7f559d9f
Bryan Armando <mamanibryan1@gmail.com>
2023-02-23 22:10:59 -0500

    Simplify by extracting same logic to another file

--- scraping/leaderboard.js
+++ scraping/leaderboard.js
@@ -1,34 +1,18 @@
-import * as cheerio from "cheerio";
-import { resolve, join } from "node:path";
-import { PRESIDENTS, TEAMS, writeDBFile } from "../db/index.js";
+import { PRESIDENTS, TEAMS } from "../db/index.js";
 // para node no podemos importar json directamente - especificacion ecma script modules
 
-const URLS = {
-  leaderBoard: "https://kingsleague.pro/clasificacion/",
+const LEADERBOARD_SELECTORS = {
+  team: { selector: "td.fs-table-text_3", typeOf: "string" },
+  wins: { selector: "td.fs-table-text_4", typeOf: "number" },
+  loses: { selector: "td.fs-table-text_5", typeOf: "number" },
+  goalsScored: { selector: "td.fs-table-text_6", typeOf: "number" },
+  goalsConceded: { selector: "td.fs-table-text_7", typeOf: "number" },
+  cardsYellow: { selector: "td.fs-table-text_8", typeOf: "number" },
+  cardsRed: { selector: "td.fs-table-text_9", typeOf: "number" },
 };
 
-async function scrape(url) {
-  const res = await fetch(url);
-  const html = await res.text();
-  return cheerio.load(html);
-}
-
-// Esto se ejecutara cada 4 horas o 3 para que se actualice
-// primero se guarda en un fichero estatico
-// despues una base de datos
-async function getLeaderBoard() {
-  const $ = await scrape(URLS.leaderBoard);
+export async function getLeaderBoard($) {
   const $rows = $("table tbody tr");
-
-  const LEADERBOARD_SELECTORS = {
-    team: { selector: "td.fs-table-text_3", typeOf: "string" },
-    wins: { selector: "td.fs-table-text_4", typeOf: "number" },
-    loses: { selector: "td.fs-table-text_5", typeOf: "number" },
-    goalsScored: { selector: "td.fs-table-text_6", typeOf: "number" },
-    goalsConceded: { selector: "td.fs-table-text_7", typeOf: "number" },
-    cardsYellow: { selector: "td.fs-table-text_8", typeOf: "number" },
-    cardsRed: { selector: "td.fs-table-text_9", typeOf: "number" },
-  };
 
   const getTeambyName = (name) => {
     const { presidentId, ...restOfTeam } = TEAMS.find(
@@ -38,7 +22,6 @@ async function getLeaderBoard() {
     const president = PRESIDENTS.find(
       (presidente) => presidente.id == presidentId
     );
-
     return { ...restOfTeam, president };
   };
 
@@ -48,8 +31,7 @@ async function getLeaderBoard() {
     const arrayTeamEntries = Object.entries(LEADERBOARD_SELECTORS).map(
       ([key, { selector, typeOf }]) => {
         const rawValue = $el.find(selector).text().trim();
-        // convertir si es numero u otro :v tipo de dato
-        // const value = isNan(Number(value)) ? value : Number(value)
+        // const value = isNaN(Number(value)) ? value : Number(value)
         const value = typeOf === "number" ? Number(rawValue) : rawValue;
         return [key, value];
       }
@@ -66,13 +48,7 @@ async function getLeaderBoard() {
 
   return leaderBoard;
 }
-const resultLeaderBoard = await getLeaderBoard();
-// creando ruta relativaa de fichero :V
+
 // cwd = current working directory - desde donde se ejecuta el script
 // path.resolve() == process.cwd() = D:\PROYECTOS\REACT\kings-league
-const filePath = join(resolve(), "db", "leaderBoard.json");
-
-await writeDBFile(filePath, resultLeaderBoard);
-// Cron Job - cronometro  programa de utilidad para repetir tareas en un momento posterior
-// cada vez q el usuario quiera llama ejecuta la funcion  work flow https://workers.cloudflare.com/
 // API EN cloudflare workers : escala al infinito - bd a traves de ficheros
---
+++ scraping/mvp.js
@@ -0,0 +1,36 @@
+import { getImageFromTeam } from "./utils.js";
+
+const MVP_SELECTORS = {
+  team: { selector: "td.fs-table-text_3", typeOf: "string" },
+  playerName: { selector: "td.fs-table-text_4", typeOf: "string" },
+  matchsPlayed: { selector: "td.fs-table-text_5", typeOf: "number" },
+  mvps: { selector: "td.fs-table-text_6", typeOf: "number" },
+};
+
+export async function getMvpPlayers($) {
+  const $rows = $("table tbody tr");
+
+  const mvpList = [];
+  $rows.each((index, row) => {
+    const $oneMvpHtml = $(row);
+    const arrayEntriesMvp = Object.entries(MVP_SELECTORS).map(
+      ([key, { selector, typeOf }]) => {
+        const rawValue = $oneMvpHtml.find(selector).text().trim();
+        const value = typeOf === "number" ? Number(rawValue) : rawValue;
+        return [key, value];
+      }
+    );
+
+    const { team: teamName, ...restOfData } =
+      Object.fromEntries(arrayEntriesMvp);
+    const image = getImageFromTeam({ name: teamName });
+
+    mvpList.push({
+      ...restOfData,
+      rank: index + 1,
+      team: teamName,
+      image,
+    });
+  });
+  return mvpList;
+}
