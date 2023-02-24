d6700d2c11d6229de7e07f3f1c79a10097bbcb62
Bryan Armando <mamanibryan1@gmail.com>
2023-02-23 22:08:43 -0500

    fix writeFile function

--- db/index.js
+++ db/index.js
@@ -1,9 +1,20 @@
-import { writeFile } from "node:fs/promises";
+import { writeFile, readFile } from "node:fs/promises";
 import teams from "../db/teams.json" assert { type: "json" };
 import presidents from "../db/presidents.json" assert { type: "json" };
+import path from "node:path";
+
+const DB_PATH = path.join(process.cwd(), "./db/");
+
+export function readDBFile(dbName) {
+  return readFile(`${DB_PATH}/${dbName}.json`, "utf-8").then(JSON.parse);
+}
 
 export const TEAMS = teams;
 export const PRESIDENTS = presidents;
 
 export const writeDBFile = (fileName, data) =>
-  writeFile(fileName, JSON.stringify(data, null, 2), "utf-8");
+  writeFile(
+    `${DB_PATH}/${fileName}.json`,
+    JSON.stringify(data, null, 2),
+    "utf-8"
+  );
